e1fe1e185afb2101e850e396990626586123437f
Mamadou MAREGA <hamshakour93@gmail.com>
2020-10-08 22:34:32 +0200

    Creating node server and installing babel express and nodemon packages

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'Slim shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'Nice product'
+        },
+        {
+            _id: '2',
+            name: 'Fit shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 90,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'Best product'
+        },
+        {
+            _id: '3',
+            name: 'Best shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 40,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'I like that product'
+        },
+        {
+            _id: '4',
+            name: 'Short shirt',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 2.5,
+            numReviews: 10,
+            description: 'Nice product'
+        },
+        {
+            _id: '5',
+            name: 'Dark shirt',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 3.5,
+            numReviews: 10,
+            description: 'Nice product'
+        },
+        {
+            _id: '6',
+            name: 'Slim shirt',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Best product'
+        }
+    ]
+};
