1cd0b228ab88d774a38108cefca37ff503f63690
Mamadou MAREGA <hamshakour93@gmail.com>
2020-10-10 21:08:16 +0200

    Cart screen advanced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Link} from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
+import CartScreen from "./screens/CartScreen";
 import ProductScreen from "./screens/ProductScreen";
 
 function App() {
@@ -41,8 +42,8 @@ function App() {
               <main className="main">
                   <div className="content">
                       <Route path="/product/:id"  component={ProductScreen} />
+                      <Route path="/cart/:id?" component={CartScreen} />
                       <Route path="/" exact={true} component={HomeScreen} />
-
                   </div>
               </main>
               <footer className="footer">
